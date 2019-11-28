159f737f0a997e6ccb2783e1df6c6bf592cca965
Andrey Dyldin <and@cesbo.com>
2019-11-29 02:01:15 +0500

    rename test file for previous commit

---
+++ tests/bitwrap.rs
@@ -0,0 +1,48 @@
+use bitwrap::*;
+
+
+#[test]
+fn test_bitwrap() {
+    const DATA: &[u8] = &[0xA5, 0x5B, 0xF5, 0x67, 0x89, 0xAF, 0xAA];
+
+    #[derive(Default, Debug, BitWrap)]
+    struct F1 {
+        #[bits(4)] skip_2: u8,
+        #[bits(12)] or_mask_test: u16,
+    }
+
+    #[derive(Default, Debug, BitWrap)]
+    struct F2 {
+        #[bits(13)] or_rshift_test: u16,
+        #[bits(3)] skip_3: u8,
+    }
+
+    #[derive(Default, Debug, BitWrap)]
+    struct Packet {
+        #[bits(6)] rshift_test: u8,
+        #[bits(4)] lshift_rshift_test: u8,
+        #[bits(6)] skip_1: u8,
+        #[bitwrap] f1: F1,
+        #[bitwrap] f2: F2,
+        #[bits(8)] tail: u8,
+    }
+
+    let mut packet = Packet::default();
+    let result = packet.unpack(DATA);
+
+    assert_eq!(result, DATA.len());
+    assert_eq!(packet.rshift_test, 0x29);
+    assert_eq!(packet.lshift_rshift_test, 0x05);
+    assert_eq!(packet.skip_1, 0x1B);
+    assert_eq!(packet.f1.skip_2, 0x0F);
+    assert_eq!(packet.f1.or_mask_test, 0x0567);
+    assert_eq!(packet.f2.or_rshift_test, 0x1135);
+    assert_eq!(packet.f2.skip_3, 0x07);
+    assert_eq!(packet.tail, 0xAA);
+
+    let mut buffer: Vec<u8> = Vec::with_capacity(256);
+    let result = packet.pack(&mut buffer);
+
+    assert_eq!(result, DATA.len());
+    assert_eq!(buffer.as_slice(), DATA);
+}
